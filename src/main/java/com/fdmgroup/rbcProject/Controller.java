76a3f53e644dca12b66bdc8d5565e503330bb7df
mpatel500 <42186500+mpatel500@users.noreply.github.com>
2018-10-15 09:29:22 +0100

    Update Controller.java

--- src/main/java/com/fdmgroup/rbcProject/Controller.java
+++ src/main/java/com/fdmgroup/rbcProject/Controller.java
@@ -12,9 +12,7 @@ public class Controller {
 				System.out.println("Invalid fruit name is selected, please try again!");
 				return 0;
 			}
-			
 			System.out.println("The price for this fruit is: "+price+" pence");
-			
 			return price;
 	}
 		
@@ -28,32 +26,24 @@ public class Controller {
 		}
 	}
 
-
 	public Double findTotalPrice(int price, int quantity, double totalPrice){
-		
 		if(quantity<0){
 			System.out.println("The quantity cannot be negative!");
 			return 0.0;
 		}
-		
 		if(quantity==0 && totalPrice > 0){
 			System.out.println("You have added nothing to your basket!");
 			return totalPrice;
 		}
-		
 		if(quantity==0 && totalPrice == 0){
 			System.out.println("You have added nothing to your basket!");
 			return 0.0;
 		}
-
-		
 		if(quantity>1000){
 			System.out.println("Please enter a number between 1 and 1000");
 			return 0.0;
 		}
-		
 		double output =  totalPrice + ( price * quantity );
 		return output;
 	}
-	
 }
